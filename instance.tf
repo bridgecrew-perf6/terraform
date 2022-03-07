a9fc157724ab1fd27d615789fcdca8e432a36da0
Farid <91734370+Farid987@users.noreply.github.com>
2022-03-07 12:31:25 +0400

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "database" {
     type     = "ssh"
     host     = self.public_ip
     user     = "ubuntu"
-    private_key = ${PRIVATE_KEY}
+    private_key = ${{ secrets.PRIVATE_KEY }}
   }
 
 
@@ -53,7 +53,7 @@ resource "aws_instance" "app" {
     type     = "ssh"
     host     = self.public_ip
     user     = "ubuntu"
-    private_key = ${PRIVATE_KEY}
+    private_key = ${{ secrets.PRIVATE_KEY }}
   }
 
 
