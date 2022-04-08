05bf8a315fe294bc6c71d007e897f76c909c3790
Farid <91734370+Farid987@users.noreply.github.com>
2022-04-08 19:32:39 +0400

    Update kanan.tf

--- kanan.tf
+++ kanan.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "testucun" {
   ami           = "ami-04505e74c0741db8d"
   instance_type = "t2.micro"
-  key_name = "faridkey" 
+  key_name = "salamkey" 
   user_data = <<EOF
 #!/bin/bash
 apt update -y
